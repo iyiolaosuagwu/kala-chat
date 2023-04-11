1ee499c33a3f09206b8580a84601a36f168b8bd2
Iyiolaosuagwu <iyiolaosuagwu1@gmail.com>
2023-04-11 20:02:32 +0100

    fix: Text component can take a function as AS

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import './App.css';
 import { Text } from './components/Text';
 
 function App() {
+
+  const Emphasis = ({children}: {children: React.ReactNode}) => {
+    return <em style={{background: 'yellow'}}>{children}</em>
+  }
+  
   return (
     <div className="App">
       <Text as="a" href="http://google.com/">Iyiola Osuagwu dd</Text>
       <Text as="h6">Iyiola Osuagwu</Text>
       <Text as="button">Iyiola Osuagwu</Text>
+      <Text as={Emphasis}>Iyiola Osuagwu</Text>
       <Text>Iyiola Osuagwu</Text>
     </div>
   );
